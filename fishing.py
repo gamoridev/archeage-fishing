8b343001fcc907a457a980fa7423be927caba52b
gamoridev <gamoridev@protonmail.com>
2021-03-26 21:11:51 -0300

    fix x width

--- fishing.py
+++ fishing.py
@@ -23,39 +23,39 @@ if is_admin():
     lastskill = None
     while keyboard.is_pressed('end') == False:
         now = datetime.now().time()
-        if pyautogui.locateOnScreen('images/up.png', region=(700, 0, 300, 150), confidence=CONFIDENCE) != None:
+        if pyautogui.locateOnScreen('images/up.png', region=(700, 0, 400, 150), confidence=CONFIDENCE) != None:
             skill = 'up'
             if lastskill != skill:
                 keyboard.press_and_release('up')
                 keyboard.press_and_release('up')
                 lastskill = skill
                 print(now, "->> SKILL seta pra CIMA")
-        elif pyautogui.locateOnScreen('images/left.png', region=(700, 0, 300, 150), confidence=CONFIDENCE) != None:
+        elif pyautogui.locateOnScreen('images/left.png', region=(700, 0, 400, 150), confidence=CONFIDENCE) != None:
             skill = 'left'
             if lastskill != skill:
                 keyboard.press_and_release('left')
                 lastskill = skill
                 print(now, "->> SKILL seta pra ESQUERDA")
-        elif pyautogui.locateOnScreen('images/right.png', region=(700, 0, 300, 150), confidence=CONFIDENCE) != None:
+        elif pyautogui.locateOnScreen('images/right.png', region=(700, 0, 400, 150), confidence=CONFIDENCE) != None:
             skill = 'right'
             if lastskill != skill:
                 keyboard.press_and_release('right')
                 lastskill = skill
                 print(now, "->> SKILL seta pra DIREITA")
-        elif pyautogui.locateOnScreen('images/pull.png', region=(700, 0, 300, 150), confidence=CONFIDENCE) != None:
+        elif pyautogui.locateOnScreen('images/pull.png', region=(700, 0, 400, 150), confidence=CONFIDENCE) != None:
             skill = 'down'
             if lastskill != skill:
                 keyboard.press_and_release('down')
                 keyboard.press_and_release('down')
                 lastskill = skill
                 print(now, "->> SKILL PUXAR peixe")
-        elif pyautogui.locateOnScreen('images/release.png', region=(700, 0, 300, 150), confidence=CONFIDENCE) != None:
+        elif pyautogui.locateOnScreen('images/release.png', region=(700, 0, 400, 150), confidence=CONFIDENCE) != None:
             skill = 'home'
             if lastskill != skill:
                 keyboard.press_and_release('home')
                 lastskill = skill
                 print(now, "->> SKILL SOLTAR peixe")
-        elif pyautogui.locateOnScreen('images/target.png', region=(700, 0, 300, 150), confidence=CONFIDENCE) != None:
+        elif pyautogui.locateOnScreen('images/target.png', region=(700, 0, 400, 150), confidence=CONFIDENCE) != None:
             if lastskill == None:
                 print(now, "->> AGUARDANDO SKILL")
                 time.sleep(0.1)
